0a27edfbd9f6a3cf4a4d274097de817f0ccd6534
Alon Rolnik <alonrolnik@gmail.com>
2021-11-21 21:32:59 +0200

    Create factory.py

---
+++ factory.py
@@ -0,0 +1,15 @@
+class AirflowDAGFactory:
+    """Factory method for getting a DAG."""
+
+    @staticmethod
+    def airflow_dag(*keys: List[Any], **kwargs: Dict[AnyStr, Any]) -> DAG:
+        """
+        Factory method for getting a DAG.
+        Been written to avoid repeating ourselves in the common patrs.
+        """
+        return DAG(
+            *keys,
+            dagrun_timeout=kwargs.pop("dagrun_timeout", timedelta(hours=DAG_RUN_DEFAULT_TIMEOUT_HOURS)),
+            user_defined_macros={"make_safe_label_value": make_safe_label_value},
+            **kwargs,
+        )
